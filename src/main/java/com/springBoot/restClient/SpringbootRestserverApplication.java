2fc19bfdbe4874356db8f2f11cc6edfb793c92ae
Yannick Bribaud <bribaudyannick@gmail.com>
2022-10-23 22:21:50 -1200

    Service Rest

---
+++ src/main/java/com/springBoot/restClient/SpringbootRestserverApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.restClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot/restClient/SpringbootRestserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.restClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
